aed6c934ad310de93567b90d9a10ac057a55f31a
Rob McKinnon <@digital.justice.gov.uk>
2014-05-19 16:14:54 +0100

    new days until endpoint, renders html output

--- app.rb
+++ app.rb
@@ -8,10 +8,24 @@ class Countdowner < Sinatra::Base
 
   get '/until/:date/:msg' do
     msg = params[:msg]
-    today = Date.today
-    end_date = Date.parse(params[:date])
-    en = HolidayCalendar.load(:uk)
-    item = { "value" => "#{en.count_working_days_between(today, end_date)}", "text" => "business days #{msg}" }
+    days = business_days_until(params[:date])
+    item = {
+      value: days,
+      text: "business days #{msg}"
+    }
+    result item
+  end
+
+  get '/days_until/:date/:title' do
+    days = business_days_until(params[:date])
+
+    item = {
+      text: %Q|<div class="widget widget-countdown assessment" data-end="16-Jun-2014 13:00:00" data-title="Alpha Assessment" data-id="assessment">
+      <h1 class="title" data-bind="title">#{params[:title]}</h1>
+      <h2 class="countdown-time" data-bind="timeleft">#{days}</h2>
+    </div>|,
+      type: 0
+    }
     result item
   end
 
@@ -24,8 +38,15 @@ class Countdowner < Sinatra::Base
 
   private
 
+  def business_days_until date
+    today = Date.today
+    end_date = Date.parse(date)
+    en = HolidayCalendar.load(:uk)
+    days = en.count_working_days_between(today, end_date)
+  end
+
   def result item
-    {"item" => [{}, item, {}] }.to_json
+    { "item" => [item] }.to_json
   end
 
   def pr_result count
